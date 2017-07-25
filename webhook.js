c3a654082311e34a37bb7ed65d6bc3dacd1bada3
Lee Warnock <clwarnock@gmail.com>
2017-07-25 18:50:39 -0500

    create GET and POST routes

--- webhook.js
+++ webhook.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
-const request = require('request');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -11,7 +10,7 @@ const server = app.listen(process.env.PORT || 5000, () => {
 
 /* For Facebook Validation */
 app.get('/webhook', (req, res) => {
-  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'correcthorsebatterystaple69!') {
+  if (req.query['hub.mode'] && req.query['hub.verify_token'] === 'tuxedo_cat') {
     res.status(200).send(req.query['hub.challenge']);
   } else {
     res.status(403).end();
@@ -32,25 +31,3 @@ app.post('/webhook', (req, res) => {
     res.status(200).end();
   }
 });
-
-// Send Message
-function sendMessage(event) {
-  let sender = event.sender.id;
-  let text = event.message.text;
-
-  request({
-    url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: EAAJnVnDoQkMBAP0RmtpRZA0sNeKVzmIGPlqTfu3jYiTvx1WuEZChdBKjKkGZCdk0uIApv1pJC4Mv7fOLqkRuTUtkf94Fa289V2P1hRpso3KrUzZAcLfjhorqSZBzrVAcQg0hljmmGhjypSL6O7B4dV2sIfB58X0qLNuFHs7hvgAZDZD},
-    method: 'POST',
-    json: {
-      recipient: {id: sender},
-      message: {text: text}
-    }
-  }, function (error, response) {
-    if (error) {
-        console.log('Error sending message: ', error);
-    } else if (response.body.error) {
-        console.log('Error: ', response.body.error);
-    }
-  });
-}
