e3818ebc97ae2f787801e8788a92f84fa335bc79
Lee Warnock <clwarnock@gmail.com>
2017-07-25 19:57:42 -0500

    adds post echo

--- webhook.js
+++ webhook.js
@@ -33,3 +33,27 @@ app.post('/', (req, res) => {
     res.status(200).end();
   }
 });
+
+//POST reply with an echo of whatever was entered into chat field
+const request = require('request');
+
+function sendMessage(event) {
+  let sender = event.sender.id;
+  let text = event.message.text;
+
+  request({
+    url: 'https://graph.facebook.com/v2.6/me/messages',
+    qs: {access_token: PAGE_ACCESS_TOKEN},
+    method: 'POST',
+    json: {
+      recipient: {id: sender},
+      message: {text: text}
+    }
+  }, function (error, response) {
+    if (error) {
+        console.log('Error sending message: ', error);
+    } else if (response.body.error) {
+        console.log('Error: ', response.body.error);
+    }
+  });
+}
